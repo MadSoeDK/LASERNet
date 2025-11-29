d159baca1e51bfe54da6aa6be6376abdee792e11
buster_bn <busternielsen99@gmail.com>
2025-11-29 11:22:22 +0100

    ready for training

--- batch/scripts/train_MICROnet_notebook.sh
+++ batch/scripts/train_MICROnet_notebook.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#BSUB -J micro-net-1
+#BSUB -J MICROnet_notebook
 #BSUB -q gpua100
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -W 2:00
+#BSUB -W 23:00
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=64GB]"
+#BSUB -R "rusage[mem=20GB]"
 ##BSUB -u s211548@dtu.dk
 #BSUB -B
 #BSUB -N
-#BSUB -o logs/micro-net_1_%J.out
-#BSUB -e logs/micro-net_1_%J.err
+#BSUB -o logs/MICROnet_notebook_%J.out
+#BSUB -e logs/MICROnet_notebook_%J.err
 
 # Load modules
 module load python3/3.12.0
